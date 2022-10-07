db2cf410625f554e0ba32b83f0f13749ed41b8f4
Yanis2002 <70938875+Yanis2002@users.noreply.github.com>
2022-10-07 17:49:20 +0300

    Update list.py

--- ДЗ 3/list.py
+++ ДЗ 3/list.py
@@ -1,11 +1,11 @@
 from itertools import *
 class FFF():
-    def __init__(self, X, Y, c):
+    def __init__(self,c:str, X:list, Y:list):
         self.X= X
         self.Y = Y
         self.c= c
 
-    def new_list(self):
+    def new_list(self,X,Y,c):
         if c == "+":
             print(X.add())
         if c == "-":
@@ -14,17 +14,18 @@ class FFF():
             print(X.mul())
         if c == "/":
             print(X.truediv())
-    class HHH():
-        def add(self,other):
-                return [x + y for x, y in zip_longest(X, Y, fillvalue=0)]
-        def sub(self, other):
-            return [x - y for x, y in zip_longest(X, Y, fillvalue=0)]
-        def mul(self, other):
-            return [x * y for x, y in zip_longest(X, Y, fillvalue=0)]
-        def truediv(self,other):
-            return [x / y for x, y in zip_longest(X, Y, fillvalue=0)]
-X= FFF((list(input())))
-Y= FFF(list(input()))
-c= FFF(list(input()))
-X,Y,c.new_list()
+    def __add__(self,other):
+            return [x + y for x, y in zip_longest(X, Y, fillvalue=0)]
+    def __sub__(self, other):
+        return [x - y for x, y in zip_longest(X, Y, fillvalue=0)]
+    def __mul__(self, other):
+        X= [x * y for x, y in zip_longest(X, Y, fillvalue=0)]
+        return X
+    def __truediv__(self,other):
+        X= [x / y for x, y in zip_longest(X, Y, fillvalue=0)]
+        return X
+X= FFF(list[input()])
+Y= FFF(list[input()])
+c= input()
+print(X)
 
